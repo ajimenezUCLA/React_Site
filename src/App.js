e3477663be4e2706fe51a0b833c800cc9fdcaaaf
ajimenezUCLA <angelvj90@gmail.com>
2019-07-04 21:22:13 -0700

    reactstrap, bootstrap, popper set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+
+
+/*
 function App() {
   return (
     <div className="App">
@@ -22,5 +40,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
