09721e48b312d54feb043ce61d43fe440e009c9c
ajimenezUCLA <angelvj90@gmail.com>
2019-07-22 22:31:40 -0700

    updated contactus page with forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,9 @@ import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
+
+
 import { DISHES } from '../shared/dishes'; // .. b/c you go up one folder
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -54,6 +57,7 @@ class Main extends Component {
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> } />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
