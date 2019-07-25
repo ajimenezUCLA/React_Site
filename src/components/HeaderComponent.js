aede5ab954e0d796c2fde08d589024478fbde5c5
ajimenezUCLA <angelvj90@gmail.com>
2019-07-24 18:38:20 -0700

    included FormFeedback with error messages to contactcomponent

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -28,29 +28,29 @@ class Header extends Component {
                             <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion"/>
                         </NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
-                    <Nav navbar>
-                        <NavItem>
-                            <NavLink className="nav-link" to="/home">
-                                <span className="fa fa-home fa-lg"></span> Home
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link" to="/aboutus">
-                                <span className="fa fa-info fa-lg"></span> About Us
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link" to="/menu">
-                                <span className="fa fa-list fa-lg"></span> Menu
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link" to="/contactus">
-                                <span className="fa fa-address-card fa-lg"></span> Contact Us
-                            </NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg"></span> Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info fa-lg"></span> About Us
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg"></span> Menu
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg"></span> Contact Us
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
                 <Jumbotron>
