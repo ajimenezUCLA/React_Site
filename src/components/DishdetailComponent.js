19bc15f26d4b012ecffabaa4ad7ee32d6eefa0dc
ajimenezUCLA <angelvj90@gmail.com>
2019-08-03 17:12:18 -0700

    included json server and fetch commands

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,7 +6,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle,
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
-
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -120,7 +120,7 @@ function RenderDish({dish}) { // props only receives one props object
     return (
         <div className="col-12 col-md-5 m-1">
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
